883a46836fbf85a43926ac03c8180670715fda87
Derek Jiang <derek.jiang917@gmail.com>
2021-05-05 23:02:21 -0700

    added answers

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import math
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,8 +17,18 @@ def send():
 
 
         if weight.isdigit() and cbodyfat.isdigit() and gbodyfat.isdigit() and rate.isdigit():
-            lean_mass = weight *(100-cbodyfat)
-            return render_template('index.html', lean_mass = lean_mass)
+            weight = float(weight)
+            cbodyfat = float(cbodyfat)
+            gbodyfat = float(gbodyfat)
+            rate = float(rate)
+            clean_mass = weight*(100-cbodyfat)/100
+            glean_mass = clean_mass*.97
+            goal_weight = glean_mass/((100-gbodyfat)/100)
+            tt6 = math.ceil((weight-goal_weight)/rate)
+            clean_mass = round(clean_mass, 2)
+            glean_mass = round(glean_mass, 2)
+            goal_weight = round(goal_weight, 2)
+            return render_template('index.html', clean_mass = clean_mass, glean_mass = glean_mass, goal_weight = goal_weight, tt6 = tt6)
         else:
             return 'Error: one or more inputs was not a number'
 
